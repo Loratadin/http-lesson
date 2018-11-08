18554b0092cf3df0d9461d480a2e94664f1d102e
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-11-08 14:11:56 -0600

    add routes for Posts and NewPost

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+import { Route } from 'react-router-dom';
 import './Blog.css';
 
 class Blog extends Component {
@@ -14,7 +16,9 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts/>
+                {/* <Route path="/" exact render={ () => <h1>Home</h1>}/> */}
+                <Route path="/" exact component={Posts}/>
+                <Route path="/new-post" component={NewPost}/>
             </div>
         );
     }
